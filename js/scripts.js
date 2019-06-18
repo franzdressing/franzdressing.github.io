c922e0b4c5773cd17d1f6da3e85655940e32ea5a
Anna <ann@myplaycity.com>
2019-06-18 22:58:09 +0400

    gifs and eggs addded

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,26 @@ $( document ).ready(function() {
     	$("#snackBowl01").show();
     	$("#snackBowl02").hide();
     	$("#snackBowl03").hide();
-    	$("#snackBar").css('background-position','-15px 0');
+    	$("#snackBar").css('background-image','url(img/bar-02.gif)');
     });
     $("#snackButton03").on('click mouseover', function() {
     	$("#snackBowl02").show();
     	$("#snackBowl03").hide();
-    	$("#snackBar").css('background-position','-30px 0');
+    	$("#snackBar").css('background-image','url(img/bar-03.gif)');
     });
     $("#snackButton04").on('click mouseover', function() {
     	$("#snackBowl03").show();
-    	$("#snackBar").css('background-position','-45px 0');
+    	$("#snackBar").css('background-image','url(img/bar-04.gif)');
     });
     $("#snackButton01").on('click mouseover', function() {
     	$("#snackBowl01").hide();
     	$("#snackBowl02").hide();
     	$("#snackBowl03").hide();
-    	$("#snackBar").css('background-position','0px 0');
+    	$("#snackBar").css('background-image','url(img/bar-01.gif)');
+    });
+
+    $("#julianSkills02").on('click', function() {
+        $("#julianEars").toggle();
+        $("#julianTail").toggle();
     });
 });
